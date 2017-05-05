98572882415ece2790b8aaa369b41e672db4c54c
Jonathon Pyles <Jonathon Pyles@DESKTOP-1MACHN2>
2017-05-04 21:33:05 -0400

    added more giberehs

--- practice_git_control/practice_git_control.cpp
+++ practice_git_control/practice_git_control.cpp
@@ -6,9 +6,17 @@
 
 int main()
 {
+	while (true) {
+		//notjing
+	}//what
 	int i = 0;
 	i = 1;
-	cout << "hello" << endl;
+	cout << "hello" << endl; //adding more 
+	//testing
     return 0;
-}
 
+
+	//adddddfdgdgdg
+}//what is this
+
+//adding more
